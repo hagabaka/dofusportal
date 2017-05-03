02816c504ee1bc43a9b1d19cd18ae2c0f7a6effc
Yaohan Chen <yaohan.chen@gmail.com>
2017-05-03 19:57:06 -0400

    Update files

--- main.js
+++ main.js
@@ -42,7 +42,7 @@
   previous_request = null;
 
   viewModel = {
-    servers: ['Echo', 'OtoMustam', 'Rushu', 'Rosal', 'Shika', 'Solar', 'Zatoishwan', 'Test'],
+    servers: ['Echo', 'OtoMustam', 'Test'],
     loadData: function(server) {
       return function(callback) {
         $('#spinner').show();
